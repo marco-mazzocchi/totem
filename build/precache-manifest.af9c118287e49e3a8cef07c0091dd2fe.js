6ed68b5f5d81a92f4f965087be3a30c3c2012152
Marco Mazzocchi <marco.mazzocchi.pc@gmail.com>
2020-01-24 18:57:52 +0100

    build client folder

---
+++ build/precache-manifest.af9c118287e49e3a8cef07c0091dd2fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb81ebf03c0d2c5fd55ee1c0c2913c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8da0ddd8cf7c6682c19",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "7ce3e4a41e38e4c1707c",
+    "url": "/static/js/2.1844482d.chunk.js"
+  },
+  {
+    "revision": "a8da0ddd8cf7c6682c19",
+    "url": "/static/js/main.e7e51a40.chunk.js"
+  },
+  {
+    "revision": "88e14269a1a43c46451f",
+    "url": "/static/js/runtime-main.abb25259.js"
+  }
+]);
