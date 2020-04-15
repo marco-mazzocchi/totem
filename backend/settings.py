26d79a4887aa60c99958f4494c70dce08413fb07
Marco Mazzocchi <marco.mazzocchi.pc@gmail.com>
2020-04-15 18:35:56 +0200

    static file settings

--- backend/settings.py
+++ backend/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "build", "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # If you want to serve user uploaded files add these settings
